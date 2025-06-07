c1aee7633e9b0c5a0b492eef3b4f9e3b40728b38
Abhishek Rathod <118195093+abhishek-rathod1@users.noreply.github.com>
2025-06-07 19:19:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,21 @@
-//your code here
+let todoItemList = [];
 
+const btn = document.getElementById('addTodoBtn');
+const input = document.getElementById('newTodoInput');
+const tdL = document.getElementById('todoList');
+
+btn.addEventListener('click', () => {
+	const todoItem = input.value.trim();
+	if (todoItem !== '') {
+		todoItemList.push(todoItem);
+		renderItem(todoItemList);
+		input.value = ''; // clear input field after adding
+	}
+});
+
+function renderItem(list) {
+	tdL.innerHTML = '';
+	list.forEach(item => {
+		tdL.innerHTML += `<li>${item}</li>`;
+	});
+}
